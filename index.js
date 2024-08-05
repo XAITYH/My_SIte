9c5325f0312e27cb5360afa46754875e2e12e394
XAITYH <petrovskijpavel1@gmail.com>
2024-08-05 17:15:34 +0300

    Nice:)

---
+++ index.js
@@ -0,0 +1,26 @@
+const copyright = document.querySelector(".copyright");
+const currentYear = new Date().getFullYear();
+copyright.innerHTML = `© 2023 - ${currentYear} Pavel Petrovskiy.`;
+
+let options = {
+    root: null,
+    rootMargin: '5px',
+    threshold: 0.5,
+};
+
+let callback = function(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log('find', entry);
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+let targets = document.querySelectorAll('.anim')
+targets.forEach(target => {
+    observer.observe(target);
+});
